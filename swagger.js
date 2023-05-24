2b1caa2970219ea7429b1f1776a581ff9171eda0
Lily Huang <l613426210@gmail.com>
2023-05-24 11:39:36 -0600

    touch up swagger

--- swagger.js
+++ swagger.js
@@ -5,8 +5,10 @@ const doc = {
     title: 'My API',
     description: 'Temple API',
   },
+  // host: 'localhost:8080',
+  // schemes: ['http'],
   host: 'cse341-spring23-j0zc.onrender.com',
-  schemes: ['http', 'https'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
